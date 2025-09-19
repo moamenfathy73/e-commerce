2ce0f29163e08b0a0d255e44c405fc0335e207df
Moamen Fathy <moamenfathy73@gmail.com>
2025-09-19 18:17:05 +0300

    update types

--- src/apis/singleProduct.ts
+++ src/apis/singleProduct.ts
@@ -1,10 +1,10 @@
-import { product } from "@/types/product.type"
+import { Product } from "@/types/product.type"
 
 
 export default async function getSingleProduct (id: string){
 
     const response = await fetch(`https://ecommerce.routemisr.com/api/v1/products/${id}`)
-    const { data } :{ data: product } = await response.json()
+    const { data } :{ data: Product } = await response.json()
 
     return data
 
